dfe6801ef068c8fdc61862040179a60aaab3d6dd
Krzysztof <krzysztof.wieczorek1998@gmail.com>
2019-08-02 12:11:28 +0200

    Added config folder with config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set("error_reporting", "true");
+error_reporting(E_ALL);
+
+$config = array(
+    "db" => array(
+        "dbName" => "simple-blog-php",
+        "username" => "root",
+        "password" => "",
+        "host" => "localhost"
+    ),
+    "urls" => array(
+        "baseUrl" => "http://127.0.0.1:8000/" //base url
+    )
+);
+
+?>
